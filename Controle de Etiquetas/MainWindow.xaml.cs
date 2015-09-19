2bdf93f5687e47eb159d3fabbf35e3a33643c164
Eduardo Vieira Mendes <eduardovmendes@gmail.com>
2015-09-19 15:09:20 -0300

    Adicionado métodos que recebe informação do código de barras por Socket e adiciona no banco de dados, fazendo também o carregamento destes dados no DataGrid

--- Controle de Etiquetas/MainWindow.xaml.cs
+++ Controle de Etiquetas/MainWindow.xaml.cs
@@ -2,7 +2,10 @@
 using System.Collections.Generic;
 using System.Data;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -22,7 +25,30 @@ namespace Controle_de_Etiquetas {
     /// </summary>
     public partial class MainWindow : Window {
 
-#region Construtores
+
+        #region Variaveis
+
+        // Defino objeto Thread
+        private Thread MinhaThread;
+
+        // Define porta
+        private int Porta = 5000;
+
+        // Define IP
+        private IPAddress Server = IPAddress.Parse("192.168.25.108");
+
+        // Defino objeto TcpListene
+        private TcpListener tcpServidor;
+
+        // Defino variável de controle da Thread
+        public bool NeedStop;
+
+        #endregion Variaveis
+
+
+
+        #region Construtores
+
         public MainWindow() {
 
             // Inicializa os componentes da janela
@@ -33,6 +59,18 @@ namespace Controle_de_Etiquetas {
 
             // Chama método que faz o carregamento do cadsatro de destinos
             CarregaDestinos();
+
+            // Chama método que faz o carregamendo do cadastro de controle
+            CarregaControle();
+
+            // Instacia Thread passando a ThreadStart
+            MinhaThread = new Thread(EscutarConexao);
+
+            // Seto thread como background
+            MinhaThread.IsBackground = true;
+
+            // Inicia a Thread
+            MinhaThread.Start();
 
         }
 
@@ -84,7 +122,150 @@ namespace Controle_de_Etiquetas {
 
         }
 
+        private void CarregaControle() {
+
+            // Limpa registros do dataGrid
+            dgControle.ItemsSource = null;
+
+            // Cria objeto de acesso ao banco de dados
+            DatabaseHelper objCarregaControle = new DatabaseHelper();
+
+            // Comando SQL
+            var SQL = "SELECT id, n_id_funcionario, d_data, t_hora, n_id_destino  FROM dados.entradas WHERE b_deletado = false ORDER BY id";
+
+            // Pega DataTable com resultado do SQL
+            DataTable result = objCarregaControle.GetDataTable(SQL);
+
+            // Seta item source do DataGrid
+            dgControle.ItemsSource = result.DefaultView;
+
+        }
+
+        private void EscutarConexao() {
+
+            // Seta controle da Thread como false
+            NeedStop = false;
+
+            try {
+                // Instâncio TcpListener
+                tcpServidor = new TcpListener(Server, Porta);
+
+                // Inicio o TcpListener
+                tcpServidor.Start();
+
+                // Defino Buffer para recebimento de mensagem
+                var bytes = new byte[256];
+
+                // Defino variavel que vai receber o texto da mensamge
+                string Mensagem = null;
+
+                // Faço laço para ficar verificando mensagem
+                while (true) {
+
+                    // Verifico se deve parar o while
+                    if (NeedStop) {
+
+                        // Try to stop Thread
+                        MinhaThread.Abort();
+
+                        // Get out off the while
+                        break;
+
+                    }
+
+                    // Perform a blocking call to accept requests.
+                    // You could also user server.AcceptSocket() here.
+                    var client = tcpServidor.AcceptTcpClient();
+
+                    // Limpa mensagem
+                    Mensagem = null;
+
+                    // Cria objeto stream para ler e escrever no socket
+                    var SocketStream = client.GetStream();
+
+                    // Variáve de controle
+                    int i;
+
+                    // Faz loop para receber o texto da mensagem enviado pelo cliente.
+                    while ((i = SocketStream.Read(bytes, 0, bytes.Length)) != 0) {
+
+                        // Traduz os bytes para uma string
+                        Mensagem = Encoding.ASCII.GetString(bytes, 0, i);
+
+                        // Cria objeto de acesso ao banco de dados
+                        DatabaseHelper objControle = new DatabaseHelper();
+
+                        // Cria dicionário com chave/valor a ser inserido no banco de dados
+                        Dictionary<string, string> dctDados = new Dictionary<string, string>();
+
+                        // Pega data do dia
+                        var dData = DateTime.Now.ToString("dd/MM/yyyy");
+
+                        // Pega hora atual
+                        var dHora = DateTime.Now.ToString("hh:mm:ss");
+
+                        // TODO - Pegar na base de dados o nome do funcionario atraves do codigo de barras recebido
+                        dctDados.Add("n_id_funcionario", Mensagem.Substring(0,3));
+
+                        // TODO - Pegar na base de dados o nome do destino através do código de barras recebido
+                        dctDados.Add("n_id_destino", Mensagem.Substring(3, 5));
+
+                        // Adiciono a data no Dicionario de dados
+                        dctDados.Add("d_data",dData);
+
+                        // Adiciono a hora no Dicionario de dados
+                        dctDados.Add("t_hora", dHora);
+
+                        // Insere os dados no banco
+                        if (objControle.Insert("dados.entradas", dctDados)) {
+
+                            // Chama o método de carregametno de dados através do Invoke porque está rodando em uma Thread diferente
+                            Dispatcher.Invoke(() => {
+
+                                // Chama método que atualiza o grid de controle
+                                CarregaControle();
+
+                            });
+
+                        }
+                        else {
+
+                            // Informa que ocorreu erro ao tentar adicionar o controle no banco de dados
+                            MessageBox.Show("Ocorreu um erro ao tentar incluir o controle no banco de dados\nPor favor, informe ao seu administrador");
+                            
+                        }
+
+                        // Define mensagem de resposta transformando de string para bytes
+                        var msg = Encoding.ASCII.GetBytes("OK");
+
+                        // Envia a resposta
+                        SocketStream.Write(msg, 0, msg.Length);
+                    }
+
+                    // Finaliza a conexao
+                    SocketStream.Close();
+                }
+            }
+
+            // Em caso de erro , apresenta a mensagem na tela
+            catch (SocketException e) {
+                MessageBox.Show("Ocorreu um erro\n" + e);
+            }
+
+            finally {
+                // Finaliza a escuta
+                tcpServidor.Stop();
+            }
+        }
+
+        private void Window_Closed(object sender, System.EventArgs e) {
+
+            // Seta valor do controle para parar a thread
+            NeedStop = true;
+
+        }
 
         #endregion Métodos
+
     }
 }
