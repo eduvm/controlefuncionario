97061d2c255147c02ab59df1fd2a9528b5108280
Eduardo Vieira Mendes <eduardovmendes@gmail.com>
2015-09-19 12:02:26 -0300

    Criado método que faz o carregamento dos funcionários na inicialização

--- Controle de Etiquetas/MainWindow.xaml.cs
+++ Controle de Etiquetas/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,13 +14,52 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+using Controle_de_Etiquetas.Helpers;
+
 namespace Controle_de_Etiquetas {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window {
+
+#region Construtores
         public MainWindow() {
+
+            // Inicializa os componentes da janela
             InitializeComponent();
+
+            // Chama método que faz o carregamento do cadastro de funcionários
+            CarregaFuncionarios();
+
         }
+
+#endregion Construtores
+
+        #region Métodos
+
+        /// <summary>
+        /// Método responsável por carregar os funcionários cadastrados no banco de dados
+        /// </summary>
+        private void CarregaFuncionarios() {
+
+            // Limpa registros do dataGrid
+            dgFuncionarios.ItemsSource = null;
+            
+            // Cria objeto de acesso ao banco de dados
+            DatabaseHelper objCarregaFuncionario  = new DatabaseHelper();
+
+            // Comando SQL
+            var SQL = "SELECT id, c_funcionario FROM dados.funcionario WHERE b_deletado = false ORDER BY id";
+
+            // Pega DataTable com resultado do SQL
+            DataTable result = objCarregaFuncionario.GetDataTable(SQL);
+
+            // Seta item source do DataGrid
+            dgFuncionarios.ItemsSource = result.DefaultView;
+
+        }
+
+
+        #endregion Métodos
     }
 }
