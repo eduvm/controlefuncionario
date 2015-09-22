742a14abeabdc780746e884b86a00b7ee4e11443
Eduardo Vieira Mendes <eduardovmendes@gmail.com>
2015-09-22 13:57:27 -0300

    Alterado métodos que carregavam e mostravam o cadastro de funcionário

---
+++ Controle de Etiquetas/Funcionario/Funcionario.cs
@@ -0,0 +1,22 @@
+﻿namespace Controle_de_Etiquetas.Funcionario {
+
+    public class Funcionario {
+
+        public string Id {
+            get;
+            set;
+        }
+
+        public string Nome {
+            get;
+            set;
+        }
+
+        public string BarCode {
+            get;
+            set;
+        }
+
+    }
+
+}
