909c395ec66c90593e3b9af0787d39b1e66cd416
Eduardo Vieira Mendes <eduardovmendes@gmail.com>
2015-09-23 09:26:17 -0300

    Alterados métodos que fazem o carregamento do controle

---
+++ Controle de Etiquetas/Controles/Controle.cs
@@ -0,0 +1,98 @@
+﻿// Criado por eduardo
+// Data: 23/09/2015
+// Solução: Controle de Etiquetas
+// Projeto:Controle de Etiquetas
+// Arquivo: Controle.cs
+// =========================================
+// Última alteração: 23/09/2015
+
+namespace Controle_de_Etiquetas.Controles {
+
+    /// <summary>
+    ///     CLasse responsável por armazenar todas as informações do cadastro de controle
+    /// </summary>
+    public class Controle {
+
+        /// <summary>
+        ///     Propriedade com o ID do controle
+        /// </summary>
+        public string Id {
+            get;
+            set;
+        }
+
+        /// <summary>
+        ///     Propriedade com o nome do funcionário
+        /// </summary>
+        public string NomeFuncionario {
+            get;
+            set;
+        }
+
+        /// <summary>
+        ///     Propriedade com o ID do funcionário
+        /// </summary>
+        public string IdFuncionario {
+            get;
+            set;
+        }
+
+        /// <summary>
+        ///     Propriedade com o Nome do Cliente
+        /// </summary>
+        public string NomeCliente {
+            get;
+            set;
+        }
+
+        /// <summary>
+        ///     Propriedade com o ID do cliente
+        /// </summary>
+        public string IdCliente {
+            get;
+            set;
+        }
+
+        /// <summary>
+        ///     Propriedade com a Data da Saída
+        /// </summary>
+        public string DataSaida {
+            get;
+            set;
+        }
+
+        /// <summary>
+        ///     Propriedade com a Hora da Saída
+        /// </summary>
+        public string HoraSaida {
+            get;
+            set;
+        }
+
+        /// <summary>
+        ///     Propriedade com a Data da Chegada
+        /// </summary>
+        public string DataChegada {
+            get;
+            set;
+        }
+
+        /// <summary>
+        ///     Propriedade com a Hora da Chegada
+        /// </summary>
+        public string HoraChegada {
+            get;
+            set;
+        }
+
+        /// <summary>
+        ///     Propriedade de controle se o registro é uma saída ou chegada
+        /// </summary>
+        public bool FlagFechado {
+            get;
+            set;
+        }
+
+    }
+
+}
---
+++ Controle de Etiquetas/Controles/ListaControles.cs
@@ -0,0 +1,24 @@
+﻿// Criado por eduardo
+// Data: 23/09/2015
+// Solução: Controle de Etiquetas
+// Projeto:Controle de Etiquetas
+// Arquivo: ListaControles.cs
+// =========================================
+// Última alteração: 23/09/2015
+
+#region Usings
+
+using System.Collections.ObjectModel;
+
+#endregion
+
+namespace Controle_de_Etiquetas.Controles {
+
+    /// <summary>
+    /// Classe que serve como lista de controles
+    /// </summary>
+    public class ListaControles : ObservableCollection<Controle> {
+
+    }
+
+}
